abd3b81c41d7b89509dfc0d4355767cd2a1640bf
Justin Mott <justinmmott@gmail.com>
2017-09-16 22:05:39 -0400

    tbh idk

--- voting.py
+++ voting.py
@@ -1,17 +1,11 @@
-#make the voter registration file usable
 import csv
 
 with open('actual_voter_registration.csv', 'w', newline='') as w:
 	swriter = csv.writer(w)
-	swriter.writerow(['voter_status'] + ['county'] + ['party'])
+	swriter.writerow(['county'] + ['party'])
 	with open('voter_registration.tsv','r',newline = '') as f:
 		reader = csv.DictReader(f,dialect = 'excel-tab')
 		for row in reader:
 			if(row['voter_status_desc'] == 'ACTIVE'):
 				swriter.writerow([row['county_desc']] + [row['party_cd']])
 		pass
-
-
-
-
-		
